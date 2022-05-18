a4890f1d3dfd1b6909ae20d11c5dd90489b4ade3
BlueberryWeb <blueberryweb7@gmail.com>
2022-05-18 13:58:03 -0500

    menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menu(){
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('header').style.display = 'none';
+}
+function cerrar(){
+    document.getElementById('header').style.display = 'flex';
+    document.getElementById('menu').style.display = 'none';
+}
