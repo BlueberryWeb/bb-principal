98df8eb8359ee376d179ed9b9b575fa39250a406
BlueberryWeb <blueberryweb7@gmail.com>
2022-06-08 14:14:00 -0500

    cambios del captcga

--- js/script.js
+++ js/script.js
@@ -6,3 +6,17 @@ function cerrar(){
     document.getElementById('header').style.display = 'flex';
     document.getElementById('menu').style.display = 'none';
 }
+$('#form').submit(function (event) {
+    event.preventDefault();
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6Lcpk1UgAAAAAEpHwSdFO-KYtfupDS0T5L_Kxrs0', { action: 'registro' }).then(function (token) {
+            $('#form').prepend('<input type="hidden" name="token" value="' + token + '">');
+            $('#form').prepend('<input type="hidden" name="action" value="registro">');
+            $('#form').unbind('submit').submit();
+        });
+    });
+});
+function enviar(){
+    document.getElementById('mensaje-agradecimiento').style.display = 'block';
+    document.getElementById('contacto').style.display = 'none';
+}
