4cc5c2e613be5a7842a53b1050b3c6ce5a4d9013
BlueberryWeb <blueberryweb7@gmail.com>
2022-12-29 13:25:42 -0600

    falta footer blanco

--- blueberry-nueva/js/scripts.js
+++ blueberry-nueva/js/scripts.js
@@ -40,12 +40,12 @@ $(function () {
         if (bodyScroll > 300) {
 
             navbar.addClass("nav-scroll");
-            logo.attr('src', '../img/logo-dark.png');
+            logo.attr('src', '../img/logo-dark.gif');
 
         } else {
 
             navbar.removeClass("nav-scroll");
-            logo.attr('src', '../img/logo-light.png');
+            logo.attr('src', '../img/logo-light.gif');
         }
     });
 
@@ -93,7 +93,7 @@ $(function () {
             $('.topnav .menu-icon .text').addClass('open');
 
             navDark.addClass("navlit");
-            logoChan.attr('src', 'img/logo-light.png');
+            logoChan.attr('src', 'img/logo-light.gif.png');
 
             window.addEventListener('scroll', noScroll);
 
@@ -104,7 +104,7 @@ $(function () {
             $('.topnav .menu-icon .text').removeClass('open');
 
             navDark.removeClass("navlit");
-            logoChan.attr('src', 'img/logo-dark.png');
+            logoChan.attr('src', 'img/logo-dark.gif');
 
             window.removeEventListener('scroll', noScroll);
         }
