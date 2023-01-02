d788be0a540af71653442eb93cee8adfea5b9ecc
BlueberryWeb <blueberryweb7@gmail.com>
2023-01-02 17:01:25 -0600

    vue

---
+++ blueberryMx/src/components/homeComponents/footerView.vue
@@ -0,0 +1,114 @@
+<template>
+    
+     <!-- ==================== Start Footer ==================== -->
+
+     <footer>
+            <div class="container">
+                <div class="row mb-75">
+                    <div class="col-lg-4">
+                        <div class="item md-mb50">
+                            <div class="title">
+                                <h5>Contáctanos</h5>
+                            </div>
+                            <ul>
+                                <li>
+                                    <span class="icon pe-7s-map-marker"></span>
+                                    <div class="cont">
+                                        <h6>Visítanos</h6>
+                                        <a href="https://goo.gl/maps/AhgmTgwyNhp2kmwt9" target="_blank">
+                                            <p>Av. Rubén Darío #586, <br/>
+                                                Guadalajara, Jalisco.</p>
+                                        </a>
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="icon pe-7s-mail"></span>
+                                    <div class="cont">
+                                        <h6>Email</h6>
+                                        <a href="mailto:hello@blueberry.mx?Subject=Solicitar%20Informes%20Blueberry">
+                                            <p>hello@blueberry.mx</p>
+                                        </a>
+                                        
+                                    </div>
+                                </li>
+                                <li>
+                                    <span class="icon pe-7s-call"></span>
+                                    <div class="cont">
+                                        <h6>Llámanos</h6>
+                                        <a href="https://api.whatsapp.com/send?phone=523313257156" target="_blank">
+                                            <p>+52 (33) 1325 7256</p>
+                                        </a> <br/>
+                                        <a href="tel:+523331093636" target="_blank">
+                                            <p>+52 (33) 3109 3636</p>
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="item md-mb50">
+                            <div class="title">
+                                <h5>Legal</h5>
+                            </div>
+                            <ul>
+                                <li>
+                                    <div class="sm-post">
+                                        <a href="terminos.html">
+                                            <p>Términos y condiciones</p>
+                                        </a>
+                                        
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="sm-post">
+                                        <a href="privacidad.html">
+                                            <p>Aviso de privacidad</p>
+                                        </a>
+                                        
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="sm-post">
+                                        <a href="cookies.html">
+                                            <p>Política de cookies</p>
+                                        </a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="item">
+                            <div class="logo">
+                                <img src="https://res.cloudinary.com/dra1bsh4u/image/upload/v1672696437/BlueberryMX/logo-light_mxvdxk.gif" alt=""/>
+                            </div>
+                            <div class="social">
+                                <a href="https://www.facebook.com/profile.php?id=100089242747257" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                                <a href="https://www.instagram.com/blueberrymx_/" target="_blank"><i class="fab fa-instagram"></i></a>
+                                <a href="https://www.tiktok.com/@blueberrymx1" target="_blank"><i class="fab fa-youtube"></i></a>
+                                <a href="https://www.linkedin.com/company/blueberrymx" class="icon"><i class="fab fa-linkedin-in"></i></a>
+                                <a href="https://vimeo.com/blueberrymx" target="_blank"><i class="fab fa-vimeo-v"></i></a>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-5">
+                        <div class="copy-right">
+                            <p>© 2023, Blueberry Todos los derechos reservados.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-5"></div>
+                    <div class="col-lg-2">
+                        <div class="justify-content-end">
+                            <img src="https://res.cloudinary.com/dra1bsh4u/image/upload/v1672696434/BlueberryMX/materialized-blueberry_elmpcq.gif" alt="Materialized by Blueberry"  class="bb-materialized"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <!-- ==================== End Footer ==================== -->
+</template>
---
+++ blueberryMx/src/views/HomeView.vue
@@ -0,0 +1,110 @@
+
+<template>
+  <HeaderView/>
+
+   <!-- ==================== Start Slider ==================== -->
+
+   <header class="slider slider-prlx fixed-slider text-center">
+        <div class="swiper-container parallax-slider">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="bg-img valign" data-background="img/index/slide-01-construyendo.jpg" data-overlay-dark="6">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-8 col-md-10">
+                                    <div class="caption center mt-30">
+                                        <h1>Construyendo <span class="color-font">negocios</span> <br> 
+                                            de <span class="color-font">valor </span> 
+                                            a través de 
+                                            <span class="color-font">productos</span>
+                                            <span class="color-font">digitales</span>
+                                            </h1>
+                                        <a href="#servicios" class="butn bord curve mt-30 pLight">
+                                            <span>Conocer más</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="bg-img valign" data-background="img/index/slide-02-estetico.jpg" data-overlay-dark="6">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-7 col-md-9">
+                                    <div class="caption center mt-30">
+                                        <h1 class="">Funcionalmente <br> <b class="color-font">estético</b> </h1>
+                                        <p>La perfecta combinación entre lo funcional y lo estético para tu E-commerce.</p>
+                                        <a href="#servicios" class="butn bord curve mt-30">
+                                            <span>Conocer más</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="bg-img valign" data-background="img/index/slide-03-intuitivo.jpg" data-overlay-dark="6">
+                        <div class="container">
+                            <div class="row justify-content-center">
+                                <div class="col-lg-7 col-md-9">
+                                    <div class="caption center mt-30">
+                                        <h1 class=""> <b class="color-font">Intuitivo,</b>  escalable y   <b class="color-font">personalizable </b></h1>
+                                        <a href="#servicios" class="butn bord curve mt-30">
+                                            <span>Conocer más</span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- slider setting -->
+            <div class="setone setwo">
+                <div class="swiper-button-next swiper-nav-ctrl next-ctrl cursor-pointer">
+                    <i class="fas fa-chevron-right"></i>
+                </div>
+                <div class="swiper-button-prev swiper-nav-ctrl prev-ctrl cursor-pointer">
+                    <i class="fas fa-chevron-left"></i>
+                </div>
+            </div>
+            <div class="swiper-pagination top botm "></div>
+
+            <div class="social-icon">
+                <a href="https://www.facebook.com/profile.php?id=100089242747257" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/blueberrymx_/" target="_blank"><i class="fab fa-instagram"></i></a>
+                <a href="#https://www.tiktok.com/@blueberrymx1" target="_blank"><i class="fab fa-youtube"></i></a>
+                <a href="https://www.linkedin.com/company/blueberrymx" target="_blank"><i class="fab fa-linkedin-in"></i></a>
+                <a href="https://vimeo.com/blueberrymx" target="_blank"><i class="fab fa-vimeo-v"></i></a>
+
+            </div>
+        </div>
+    </header>
+
+    <!-- ==================== End Slider ==================== -->
+
+
+
+
+    <div class="main-content">
+      <FooterView/>
+    </div>
+
+</template>
+
+<script>
+  import { defineAsyncComponent } from 'vue';
+
+  export default{
+    components:{
+      HeaderView: defineAsyncComponent(() => import('../components/homeComponents/headerView.vue')),
+      FooterView: defineAsyncComponent(() => import('../components/homeComponents/footerView.vue')),
+      LoadingView: defineAsyncComponent(() => import('../components/homeComponents/loaderView.vue'))
+    }
+  }
+</script>
+
